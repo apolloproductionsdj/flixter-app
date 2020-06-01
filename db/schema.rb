1cfcf25f98d4e5bb3739582d347550adac236c52
Mike Torres <apolloproductionsdj@gmail.com>
2020-06-01 22:21:32 +0000

    added carrierwave and listed videos on show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_163439) do
+ActiveRecord::Schema.define(version: 2020_06_01_151515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_163439) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
